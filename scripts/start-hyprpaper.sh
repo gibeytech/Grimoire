2a7c1e9562314da47eb211004cb645dd512acc04
jonathanbourriauddev <jonathan.bourriauddev@gmail.com>
2026-05-22 20:34:10 +0200

    wallpapers par defaut

--- scripts/start-hyprpaper.sh
+++ scripts/start-hyprpaper.sh
@@ -2,7 +2,7 @@
 sleep 2
 hyprpaper &
 sleep 1
-hyprctl hyprpaper preload "/home/joe/Pictures/wallpapers/grimoire.png"
+hyprctl hyprpaper preload "/home/joe/Pictures/wallpapers/grimoire 7.png"
 sleep 0.5
-hyprctl hyprpaper wallpaper "eDP-1,/home/joe/Pictures/wallpapers/grimoire.png"
-hyprctl hyprpaper wallpaper "HDMI-A-1,/home/joe/Pictures/wallpapers/grimoire.png"
+hyprctl hyprpaper wallpaper "eDP-1,/home/joe/Pictures/wallpapers/grimoire 7.png"
+hyprctl hyprpaper wallpaper "HDMI-A-1,/home/joe/Pictures/wallpapers/grimoire 7.png"
