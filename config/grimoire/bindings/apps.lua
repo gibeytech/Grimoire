179e496e4729518feca4c458979467d0db4d941e
gibeytech <gibeytech@ik.me>
2026-06-01 14:43:20 +0200

    feat(workflow): ajout du lanceur de commandes rofi

--- config/grimoire/bindings/apps.lua
+++ config/grimoire/bindings/apps.lua
@@ -1,6 +1,7 @@
 local mainMod = "SUPER"
 
 hl.bind(mainMod .. " + Return", hl.dsp.exec_cmd(Apps.terminal))
+hl.bind(mainMod .. " + R", hl.dsp.exec_cmd("rofi -show run"))
 hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(Apps.fileManager))
 hl.bind(mainMod .. " + Space", hl.dsp.exec_cmd(Apps.launcher))
 hl.bind(mainMod .. " + B", hl.dsp.exec_cmd(Apps.browser))
