90585afc58bf6b4bb4102ce8ae9b33e617494bd1
gibeytech <gibeytech@ik.me>
2026-06-01 16:11:32 +0200

    refactor(V2): recentrage du dépot sur le noyaux Grimore

--- scripts/start-hyprpaper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sleep 2
-hyprpaper &
-sleep 1
-hyprctl hyprpaper preload "/home/joe/Pictures/wallpapers/grimoire 7.png"
-sleep 0.5
-hyprctl hyprpaper wallpaper "eDP-1,/home/joe/Pictures/wallpapers/grimoire 7.png"
-hyprctl hyprpaper wallpaper "HDMI-A-1,/home/joe/Pictures/wallpapers/grimoire 7.png"
