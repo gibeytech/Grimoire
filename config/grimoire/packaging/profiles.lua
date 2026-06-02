7d8e0c80f68ade73f524cb2b5b65d9bfbe85de27
gibeytech <gibeytech@ik.me>
2026-06-02 12:33:28 +0200

    feat(packaging): ajout des profils utilisateur

---
+++ config/grimoire/packaging/profiles.lua
@@ -0,0 +1,19 @@
+local Profiles = {}
+
+Profiles.beginner = {
+    terminal = "ghostty",
+    shell = "fish",
+    browser = "brave",
+    editor = "vscode",
+}
+
+Profiles.advanced = {
+    terminal = "ghostty",
+    shell = "fish",
+    browser = "brave",
+    editor = "neovim",
+}
+
+Profiles.custom = {}
+
+return Profiles
