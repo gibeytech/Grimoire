62f104936c9e10d70e95a539029aba39c45ee4f5
gibeytech <gibeytech@ik.me>
2026-06-02 12:51:07 +0200

    feat(packaging): résolution des profils via le catalogue

---
+++ config/grimoire/packaging/setup.lua
@@ -0,0 +1,19 @@
+local Setup = {}
+
+local Profiles = require("packaging.profiles")
+local Catalogue = require("packaging.catalogue")
+
+function Setup.new(profile)
+    local profile_data = Profiles[profile]
+
+    return {
+        profile = profile,
+
+        terminal = Catalogue.terminals[profile_data.terminal],
+        shell = Catalogue.shells[profile_data.shell],
+        browser = Catalogue.browsers[profile_data.browser],
+        editor = Catalogue.editors[profile_data.editor],
+    }
+end
+
+return Setup
