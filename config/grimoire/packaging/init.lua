6a4a972b711dfd79ea0d0539465512ed8bcd6eb4
gibeytech <gibeytech@ik.me>
2026-06-02 12:39:25 +0200

    feat(packaging): initialize packaging module

---
+++ config/grimoire/packaging/init.lua
@@ -0,0 +1,7 @@
+local Packaging = {}
+
+Packaging.catalogue = require("packaging.catalogue")
+Packaging.profiles = require("packaging.profiles")
+Packaging.layers = require("packaging.layers")
+
+return Packaging
