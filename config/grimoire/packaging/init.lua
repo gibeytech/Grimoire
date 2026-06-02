06a180ba4fbd7dc8e8a694346f570584a46f294d
gibeytech <gibeytech@ik.me>
2026-06-02 13:01:58 +0200

    feat(packaging): connecte le moteur setup au module packaging

--- config/grimoire/packaging/init.lua
+++ config/grimoire/packaging/init.lua
@@ -3,5 +3,6 @@ local Packaging = {}
 Packaging.catalogue = require("packaging.catalogue")
 Packaging.profiles = require("packaging.profiles")
 Packaging.layers = require("packaging.layers")
+Packaging.setup = require("packaging.setup")
 
 return Packaging
