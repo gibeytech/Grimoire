7523586b3d7ef7bc7c1e7e60d235890f5a4d3b47
gibeytech <gibeytech@ik.me>
2026-06-02 16:37:16 +0200

    feat(installer): ajout du moteur de rapport

--- config/grimoire/installer/init.lua
+++ config/grimoire/installer/init.lua
@@ -1,8 +1,9 @@
 local Installer = {}
 
 Installer.builder = require("installer.builder")
-Installer.preview = require("installer.preview")
-Installer.execute = require("installer.execute")
 Installer.validate = require("installer.validate")
+Installer.preview = require("installer.preview")
+Installer.report = require("installer.report")
+Installer.execute = require("installer.execute")
 
 return Installer
---
+++ config/grimoire/installer/report.lua
@@ -0,0 +1,58 @@
+local Report = {}
+
+local Packaging = require("packaging")
+
+function Report.generate(config)
+    local split = Packaging.setup.split_packages(config)
+
+    local lines = {}
+
+    table.insert(lines, "=================================")
+    table.insert(lines, " Grimoire V2 Installation Report ")
+    table.insert(lines, "=================================")
+    table.insert(lines, "")
+
+    table.insert(lines, "Profile")
+    table.insert(lines, "-------")
+    table.insert(lines, config.profile)
+    table.insert(lines, "")
+
+    table.insert(lines, "Applications")
+    table.insert(lines, "------------")
+    table.insert(lines, config.terminal.name)
+    table.insert(lines, config.shell.name)
+    table.insert(lines, config.browser.name)
+    table.insert(lines, config.editor.name)
+    table.insert(lines, "")
+
+    if config.layers.developer then
+        table.insert(lines, "Developer Layer")
+        table.insert(lines, "---------------")
+        table.insert(lines, config.layers.developer.name)
+        table.insert(lines, "")
+    end
+
+    if config.layers.containers then
+        table.insert(lines, "Container Layer")
+        table.insert(lines, "---------------")
+        table.insert(lines, config.layers.containers.name)
+        table.insert(lines, "")
+    end
+
+    if config.layers.virtualization then
+        table.insert(lines, "Virtualization Layer")
+        table.insert(lines, "-------------------")
+        table.insert(lines, config.layers.virtualization.name)
+        table.insert(lines, "")
+    end
+
+    table.insert(lines, "Packages")
+    table.insert(lines, "--------")
+    table.insert(lines, "Total    : " .. (#split.official + #split.aur))
+    table.insert(lines, "Official : " .. #split.official)
+    table.insert(lines, "AUR      : " .. #split.aur)
+
+    return table.concat(lines, "\n")
+end
+
+return Report
