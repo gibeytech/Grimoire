179b0b8622f089c95c897ef94ce1ab0a280f63c6
gibeytech <gibeytech@ik.me>
2026-06-03 10:56:17 +0200

    feat(packaging): introduce core installation layer

---
+++ config/grimoire/packaging/core.lua
@@ -0,0 +1,35 @@
+local Core = {}
+
+Core.packages = {
+    {
+        name = "Git",
+        package = "git",
+    },
+
+    {
+        name = "OpenSSH",
+        package = "openssh",
+    },
+
+    {
+        name = "Hyprpicker",
+        package = "hyprpicker",
+    },
+
+    {
+        name = "wl-clipboard",
+        package = "wl-clipboard",
+    },
+
+    {
+        name = "grim",
+        package = "grim",
+    },
+
+    {
+        name = "slurp",
+        package = "slurp",
+    },
+}
+
+return Core
--- config/grimoire/packaging/init.lua
+++ config/grimoire/packaging/init.lua
@@ -3,6 +3,7 @@ local Packaging = {}
 Packaging.catalogue = require("packaging.catalogue")
 Packaging.profiles = require("packaging.profiles")
 Packaging.layers = require("packaging.layers")
+Packaging.core = require("packaging.core")
 Packaging.setup = require("packaging.setup")
 
 return Packaging
