1d7edba9c5d0cf342abfb77dc0ba833e27262a0b
gibeytech <gibeytech@ik.me>
2026-06-05 10:19:37 +0200

    fix(packaging): ajout du Desktop Core au pack principal

--- config/grimoire/packaging/core.lua
+++ config/grimoire/packaging/core.lua
@@ -1,11 +1,55 @@
 local Core = {}
 
+local Core = {}
+
 Core.packages = {
+    -- Desktop Core
+    {
+        name = "Hyprland",
+        package = "hyprland",
+    },
+
+    {
+        name = "Waybar",
+        package = "waybar",
+    },
+
+    {
+        name = "Rofi Wayland",
+        package = "rofi-wayland",
+    },
+
+    {
+        name = "SwayNC",
+        package = "swaync",
+    },
+
+    {
+        name = "Hyprlock",
+        package = "hyprlock",
+    },
+
+    {
+        name = "Hypridle",
+        package = "hypridle",
+    },
+
+    {
+        name = "Wlogout",
+        package = "wlogout",
+    },
+
+    {
+        name = "swww",
+        package = "swww",
+    },
+
     {
         name = "Hyprpicker",
         package = "hyprpicker",
     },
 
+    -- Workflow Core
     {
         name = "wl-clipboard",
         package = "wl-clipboard",
