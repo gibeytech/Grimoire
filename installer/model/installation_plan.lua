4f4290f30e918ba77e7f4012d75c6c99c0731376
gibeytech <gibeytech@ik.me>
2026-07-09 08:50:58 +0200

    feat(installer): ajoute le modele InstallationPlan (RC1-01)

---
+++ installer/model/installation_plan.lua
@@ -0,0 +1,44 @@
+local InstallationPlan = {}
+InstallationPlan.__index = InstallationPlan
+
+function InstallationPlan:new(data)
+    assert(type(data) == "table", "InstallationPlan:new() attend une table.")
+
+    local plan = setmetatable({}, self)
+
+    plan.profile = data.profile or {}
+
+    plan.packages = data.packages or {}
+    plan.services = data.services or {}
+    plan.shell = data.shell or {}
+    plan.theme = data.theme or {}
+    plan.assets = data.assets or {}
+
+    return plan
+end
+
+function InstallationPlan:getProfile()
+    return self.profile
+end
+
+function InstallationPlan:getPackages()
+    return self.packages
+end
+
+function InstallationPlan:getServices()
+    return self.services
+end
+
+function InstallationPlan:getShell()
+    return self.shell
+end
+
+function InstallationPlan:getTheme()
+    return self.theme
+end
+
+function InstallationPlan:getAssets()
+    return self.assets
+end
+
+return InstallationPlan
